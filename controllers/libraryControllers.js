325bc3f78689d052ad488eeedf63e01cdab5d137
Uriah <uriakari@gmail.com>
2024-10-07 14:27:07 +0000

    created get library routes

---
+++ controllers/libraryControllers.js
@@ -0,0 +1,15 @@
+
+// create routes to read a specific book by ID
+
+export const readAbookById = (req, res, next) => {
+    try {
+        res.status.json('The book Id is')
+        
+    } catch (error) {
+        next(error)
+    }
+    
+
+
+} 
+
