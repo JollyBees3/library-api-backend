dc204a05e9574429fda06709dfd2a8dc42239cc1
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-10-10 14:26:36 +0000

    cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import authorRouter from "./routes/authorRoutes.js";
 import bookRouter from "./routes/bookRoutes.js";
+import cors from "cors";
 
-await mongoose.connect(process.env.MONGO_URI)
+await mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use(authorRouter);
-app.use(bookRouter)
+app.use(bookRouter);
 
 app.listen(3000, () => {
   console.log("ports are listening 3000");
